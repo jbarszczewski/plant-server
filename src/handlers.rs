a376ad3d8c963440aa2e302bb9dc6da232de2a5d
jbarszczewski <jakub.barszczewski@outlook.com>
2020-06-11 17:38:14 +0100

    getting log by ID

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,6 +3,7 @@ use bson::{doc, oid::ObjectId, Bson, UtcDateTime};
 use chrono::prelude::*;
 use futures::stream::StreamExt;
 use mongodb::{options::ClientOptions, options::FindOptions, Client};
+use rustc_serialize::hex::FromHex;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -10,9 +11,9 @@ use std::sync::Mutex;
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Log {
-    #[serde(skip_serializing_if = "Option::is_none")]
+    //   #[serde(skip_serializing_if = "Option::is_none")]
     #[serde(rename = "_id")] // Use MongoDB's special primary key field name when serializing
-    pub id: Option<ObjectId>,
+    pub id: ObjectId,
     #[serde(rename = "deviceId")]
     pub device_id: String,
     pub message: String,
@@ -23,9 +24,11 @@ const MONGO_DB: &'static str = "iotPlantDB";
 const MONGO_COLL_LOGS: &'static str = "logs";
 
 pub async fn get_logs(data: web::Data<Arc<Mutex<Client>>>) -> impl Responder {
-    let client = data.lock().unwrap();
-    let iot_db = client.database(MONGO_DB);
-    let logs_collection = iot_db.collection(MONGO_COLL_LOGS);
+    let logs_collection = data
+        .lock()
+        .unwrap()
+        .database(MONGO_DB)
+        .collection(MONGO_COLL_LOGS);
 
     let filter = doc! { "deviceId": "test_device_1" };
     //   let find_options = FindOptions::builder().sort(doc! { "timestamp": 1 }).build();
@@ -36,17 +39,46 @@ pub async fn get_logs(data: web::Data<Arc<Mutex<Client>>>) -> impl Responder {
     while let Some(result) = cursor.next().await {
         match result {
             Ok(document) => {
-                println!("document {}", document);
+                // let log: Log = bson::from_bson(Bson::Document(document)).unwrap();
                 results.push(document);
             }
-            _ => {}
+            _ => {
+                return HttpResponse::InternalServerError().finish();
+            }
         }
     }
     HttpResponse::Ok().json(results)
 }
 
-pub async fn get_log_by_id() -> impl Responder {
-    HttpResponse::Ok().body("Getting log by id not implemented yet!")
+pub async fn get_log_by_id(
+    data: web::Data<Arc<Mutex<Client>>>,
+    log_id: web::Path<String>,
+) -> impl Responder {
+    let logs_collection = data
+        .lock()
+        .unwrap()
+        .database(MONGO_DB)
+        .collection(MONGO_COLL_LOGS);
+    let bytes = log_id.from_hex().unwrap();
+    let mut byte_array: [u8; 12] = [0; 12];
+    for i in 0..12 {
+        byte_array[i] = bytes[i];
+    }
+    match logs_collection
+        .find_one(doc! { "_id":ObjectId::with_bytes(byte_array)}, None)
+        .await
+    {
+        Ok(result) => match result {
+            Some(document) => return HttpResponse::Ok().json(document),
+            None => {
+                return HttpResponse::NotFound().body(format!("No log found with id: {}", log_id))
+            }
+        },
+        Err(err) => {
+            println!("Failed! {}", err);
+            return HttpResponse::InternalServerError().finish();
+        }
+    }
 }
 
 pub async fn add_log(
@@ -54,21 +86,22 @@ pub async fn add_log(
     device_id: String,
     message: String,
 ) -> impl Responder {
-    let client = data.lock().unwrap();
-    let iot_db = client.database(MONGO_DB);
-    let logs_collection = iot_db.collection(MONGO_COLL_LOGS);
+    let logs_collection = data
+        .lock()
+        .unwrap()
+        .database(MONGO_DB)
+        .collection(MONGO_COLL_LOGS);
 
     match logs_collection.insert_one(doc! {"deviceId": device_id, "message": message, "timestamp": Bson::UtcDatetime(Utc::now())}, None).await {
         Ok(db_result) => {
             if let Some(new_id) = db_result.inserted_id.as_object_id() {
-                println!("New document inserted with id {}", new_id);            
-                //HttpResponse::Ok().json(new_id)
+                println!("New document inserted with id {}", new_id);   
             }
             return HttpResponse::Created().json(db_result.inserted_id)
         }
         Err(err) =>
         {
-            println!("Failed!");
+            println!("Failed! {}", err);
             return HttpResponse::InternalServerError().finish()
         }
     }

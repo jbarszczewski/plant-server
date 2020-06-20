4757a56736af78b7a1d63bc6ddddf21b71aa75ef
jbarszczewski <jakub.barszczewski@outlook.com>
2020-06-20 15:06:04 +0100

    moved connection string to env var

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use actix_web::{web, App, HttpServer};
 use mongodb::{options::ClientOptions, Client};
+use std::env;
 use std::sync::*;
 
 mod logs_handlers;
 
-const MONGO_URL: &'static str = "mongodb://free-tier-db:yfQtNbXyW2h9HUOOplCeHgjzzbJMnfMQn2BZuzAkw5gv0uBkqbdbQPdnQ98e6UtS5Z3p1ZrG4rgkmEKBURNgwg==@free-tier-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@free-tier-db@";
-
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=debug");
-    let mut client_options = ClientOptions::parse(MONGO_URL).await.unwrap();
+    env::set_var("RUST_LOG", "actix_web=debug");
+    let mongo_url = env::var("CONNECTION_STRING_LOGS").unwrap();
+    println!("Using connection string: {}", mongo_url);
+    let mut client_options = ClientOptions::parse(&mongo_url).await.unwrap();
     client_options.app_name = Some("PlantApi".to_string());
     let client = web::Data::new(Mutex::new(Client::with_options(client_options).unwrap()));
 
